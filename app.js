8fac09a1167d0694fe0f3b823b60fd3189e9ced4
aryansinghh1 <aryansinghkush2522@gmail.com>
2026-02-05 18:16:52 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+import express form "express";
+
+import userRouter from "./routes/userRoutes";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/api/users",userRouter);
+
+app.listen(3000,()=>{
+    console.log("server is running on port 3000");
+})
